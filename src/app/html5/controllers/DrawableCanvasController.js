295365b81df830a8eb099ec4e3859d8eb5855bbc
cody <codyallenbrooks12@yahoo.com>
2016-06-23 22:15:13 -0700

    Added DrawableCanvasController.

---
+++ src/app/html5/controllers/DrawableCanvasController.js
@@ -0,0 +1,40 @@
+/**
+ * Created by cody on 6/23/16.
+ */
+
+(function () {
+	"use strict";
+	
+	angular
+		.module('CodyPortfolio')
+		.controller('DrawableCanvasController', DrawableCanvasController);
+	
+	DrawableCanvasController.$inject = [];
+	
+	/* @ngInject */
+	function DrawableCanvasController () {
+		/* jshint validthis: true */
+		var vm = this;
+		
+		vm.activate = activate;
+		vm.title = 'DrawableCanvasController';
+		vm.onDrawableCanvasReady = onDrawableCanvasReady;
+		
+		activate();
+		
+		////////////////
+		
+		function activate () { }
+
+		/**
+		 * @param {DrawableCanvas} drawableCanvas
+		 */
+		function onDrawableCanvasReady (drawableCanvas) {
+			var rect = new DrawableCanvas.Rectangle(5, 5, 240, 240, "red");
+			var circle = new DrawableCanvas.Circle(125/2, 125/2, 125, "blue");
+			circle.filled = true;
+			drawableCanvas.setDrawables([rect, circle]);
+		}
+	}
+})();
+
